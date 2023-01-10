7ea1595088a4b15f808784bcbfd26c65d9ba9d3e
Mohammad <mhamed.mussa@gmail.com>
2023-01-09 23:45:00 -0600

    project setup

---
+++ src/components/Home/NavBarLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBarLogin() {
+  return (
+    <div>
+     Nav
+    </div>
+  )
+}
+
+export default NavBarLogin
---
+++ src/pages/Home/HomePgame.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBarLogin from '../../components/Home/NavBarLogin'
+import Slider from '../../components/Home/Slider'
+
+function HomePgame() {
+  return (
+    <div className='font'>
+     <NavBarLogin />
+     <Slider />
+    </div>
+  )
+}
+
+export default HomePgame
